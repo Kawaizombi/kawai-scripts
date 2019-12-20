27075267ccfd60a2a2c33897a7cc655425c488ea
andrey.anpilogov <andrey.anpilogov@together.com>
2019-12-20 14:35:25 +0200

    Enable angular prod mode for animespirit-ost-downloader.user.js

--- projects/animespirit-ost-downloader/index.ts
+++ projects/animespirit-ost-downloader/index.ts
@@ -1,6 +1,9 @@
 import './polyfills';
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./src/components/app/app.module";
+import { enableProdMode } from '@angular/core';
+
+if (process.env.NODE_ENV === 'production') enableProdMode();
 
 const mountPoint = document.createElement('app');
 document.querySelector('.accordion').prepend(mountPoint);
