d97191d1fb7a21e60952a353d9323d14c2eb4808
andrey.anpilogov <andrey.anpilogov@together.com>
2020-01-10 18:23:27 +0200

    Add id3 writer

--- projects/soundcloud-downloader/src/modules/popup/popup.component.ts
+++ projects/soundcloud-downloader/src/modules/popup/popup.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { ApiService } from '../core/api/api.service';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { forkJoin } from 'rxjs';
 import combineBuffers from '../../utils/combine-buffers';
 import saveFile from '@kawai-scripts/save-file';
+import ID3Writer from 'browser-id3-writer';
 
 @Component({
   selector: 'downloader-popup',
@@ -26,19 +27,41 @@ export class PopupComponent {
   }
 
   download() {
+    let metadata;
+
     this.api
       .resolveTrackByUrl(document.location.href)
       .pipe(
         switchMap(({ id }) => this.api.getTracksMetadata(id)),
+        tap(([meta]) => {
+          metadata = meta;
+        }),
         map(([{ media }]) => media.transcodings[0].url),
         switchMap((url) => this.api.getPlaylistUrl(url)),
         switchMap((url) => this.api.getPlaylist(url)),
         map((manifest) => manifest.segments.map(({ uri }) => uri)),
         switchMap((urls: string[]) => forkJoin(urls.map((url) => this.api.downloadFile(url)))),
         map(combineBuffers),
+        switchMap((buffer) => this.api
+          .downloadFile(metadata.artwork_url.replace(/(large)(\..+)$/, `t500x500$2`))
+          .pipe(
+            map((artwork) => {
+              return new ID3Writer(buffer)
+                .setFrame('TCON', metadata.genre.split(' & '))
+                .setFrame('TIT2', metadata.title)
+                .setFrame('TPE1', [metadata.user.username])
+                .setFrame('APIC', {
+                  type: 18,
+                  data: artwork,
+                  description: 'Artwork',
+                })
+                .addTag() as ArrayBuffer;
+            }),
+          ),
+        ),
       )
       .subscribe((buffer) => {
-        saveFile(new Blob([buffer]), 'test.mp3');
+        saveFile(new Blob([buffer]), `${metadata.title}.mp3`);
       });
   }
 }
