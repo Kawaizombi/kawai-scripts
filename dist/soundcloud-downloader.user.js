0e14246b76275641b01b3837fc3dc6c0af0448ca
Kawaizombi <angilogov.a98@gmail.com>
2020-02-04 15:22:13 +0200

    soundcloud-downloader.user.js v1.1.1
    Fix description

--- dist/soundcloud-downloader.user.js
+++ dist/soundcloud-downloader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        @kawai-scripts/soundcloud-downloader
-// @version     1.1.0
-// @description Add ability to download and track or playlist from www.soundcloud.com
+// @version     1.1.1
+// @description Add ability to download track or playlist from www.soundcloud.com
 // @homepage    https://github.com/Kawaizombi/kawai-scripts/tree/master/projects/youtube-blocker
 // @supportURL  https://github.com/Kawaizombi/kawai-scripts/issues
 // @match       *://www.soundcloud.com/*
